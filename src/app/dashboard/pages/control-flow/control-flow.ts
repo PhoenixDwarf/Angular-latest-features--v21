292d4f0c79b39874edff8a5c07bd5bcdaf83b5ec
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2026-01-23 12:23:21 -0500

    feat: enhance control flow component with conditional rendering and toggle functionality

--- src/app/dashboard/pages/control-flow/control-flow.ts
+++ src/app/dashboard/pages/control-flow/control-flow.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-control-flow',
@@ -10,4 +10,9 @@ import { Component } from '@angular/core';
     }
   `,
 })
-export default class ControlFlow {}
+export default class ControlFlow {
+  public showContent = signal(false);
+  public toggleContent() {
+    this.showContent.update((value) => !value);
+  }
+}
