be6a22a0fa1f8dcb2d7ac839cfcacad3a0a7e692
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2026-01-27 15:28:17 -0500

    feat: implement defer-options component with heavy loaders and title integration

--- src/app/shared/heavy-loaders/heavy-loaders-fast.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-fast',
-  imports: [],
-  styles: ``,
-  template: ` <h1>Hello world</h1> `,
+  imports: [NgClass],
+  template: `
+    <section
+      [ngClass]="['w-full flex justify-center items-center', cssClass()]"
+    >
+      <ng-content #name />
+    </section>
+  `,
 })
-export class HeavyLoadersFast {}
+export class HeavyLoadersFast {
+  public cssClass = input.required<string>();
+
+  constructor() {
+    console.log('Heavy loader fast component created!');
+  }
+}
