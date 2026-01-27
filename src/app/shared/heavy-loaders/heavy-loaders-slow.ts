a14002728ce52ae58634330bbdeed3aa279086ee
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2026-01-27 15:09:40 -0500

    feat: enhance defer-views component with loading placeholders and integrate heavy loaders

--- src/app/shared/heavy-loaders/heavy-loaders-slow.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
-  imports: [],
+  imports: [NgClass],
   styles: ``,
-  template: ` <h1>Hello world</h1> `,
+  template: `
+    <section
+      [ngClass]="['w-full h-150 flex justify-center items-center', cssClass()]"
+    >
+      Heavy Loader Slow
+    </section>
+  `,
 })
-export class HeavyLoadersSlow {}
+export class HeavyLoadersSlow {
+  public cssClass = input.required<string>();
+
+  // Implementation of a JS blocking component
+  // The following code is only for demonstration, it should never be implemented in any real project
+
+  constructor() {
+    const start = Date.now();
+    console.log('JS blocking started');
+    while (Date.now() - start < 3000) {}
+    console.log('Done blocking');
+  }
+}
