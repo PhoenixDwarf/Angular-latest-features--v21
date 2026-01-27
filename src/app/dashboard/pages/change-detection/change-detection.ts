1192d9eb4dd9c7d83789c4fb7672104f0d59a47e
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2026-01-27 14:43:38 -0500

    feat: refactor change detection component with updated imports and template structure

--- src/app/dashboard/pages/change-detection/change-detection.ts
+++ src/app/dashboard/pages/change-detection/change-detection.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+import { Title } from '@shared/title/title';
 
 @Component({
   selector: 'app-change-detection',
-  imports: [],
-  templateUrl: './change-detection.html',
-  styles: `
-    :host {
-      display: block;
-    }
+  imports: [Title, JsonPipe],
+  styles: ``,
+  template: `
+    <app-title [title]="currentFramework()" />
+    <pre>{{ frameworkAsSignal() | json }}</pre>
+    <pre>{{ frameworkAsProperty | json }}</pre>
   `,
 })
-export default class ChangeDetection {}
+export default class ChangeDetection {
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  public currentFramework = computed(
+    () => `Change detection - ${this.frameworkAsSignal().name}`,
+  );
+
+  constructor() {
+    setTimeout(() => {
+      this.frameworkAsProperty.name = 'React';
+      this.frameworkAsSignal.update((val) => ({ ...val, name: 'React' }));
+      console.log('Done');
+    }, 3000);
+  }
+}
