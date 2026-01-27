b1d102a0473b5a39f6065806c5cc56b55bdd330f
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2026-01-27 16:05:38 -0500

    feat: update view transition components with improved layout and view transition styles

--- src/app/dashboard/pages/view-transition/view-transition1.ts
+++ src/app/dashboard/pages/view-transition/view-transition1.ts
@@ -13,9 +13,13 @@ import { Title } from '@shared/title/title';
         alt="Picsum"
         width="200"
         height="300"
+        style="view-transition-name: hero1;"
       />
 
-      <div class="bg-blue-500 w-56 h-56"></div>
+      <div
+        class="bg-blue-500 w-56 h-56"
+        style="view-transition-name: hero2;"
+      ></div>
     </section>
   `,
 })
--- src/app/dashboard/pages/view-transition/view-transition2.ts
+++ src/app/dashboard/pages/view-transition/view-transition2.ts
@@ -7,15 +7,19 @@ import { Title } from '@shared/title/title';
   template: `
     <app-title title="View Transition #2" />
 
-    <section class="flex justify-start">
+    <section class="flex justify-end">
       <img
         srcset="https://picsum.photos/id/237/200/300"
         alt="Picsum"
         width="200"
         height="300"
+        style="view-transition-name: hero1;"
       />
 
-      <div class="bg-blue-500 w-56 h-56"></div>
+      <div
+        class="bg-blue-800 w-32 h-32 rounded fixed bottom-16 right-10"
+        style="view-transition-name: hero2;"
+      ></div>
     </section>
   `,
 })
